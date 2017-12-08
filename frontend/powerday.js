486ad034b3d44ef302eae0c22b8218e2a77922dc
Benjamin Deering <tr0l@localhost.localdomain>
2017-12-08 17:16:37 -0500

    Allow display of power data for dates other than today

--- frontend/powerday.js
+++ frontend/powerday.js
@@ -1,5 +1,6 @@
 
 var serial = "";
+var date = "";
 var powerdata = {};
 var powerChart = {};
 powerdata.datasets=[];
@@ -9,71 +10,66 @@ powerdata.datasets[0]={};
 powerdata.datasets[0].backgroundColor = "#9A6759";
 powerdata.datasets[0].borderColor = "#70A4B2";
 
-$(document).ready(function()
-{
-	serial = document.location.search.substr(8,99);
-	powerdata.datasets[0].label = serial;
-	addPowerChart();
-	hideError();
-	setInterval(updatePowerChart,300000); // new data available every 5 minutes
-}
-);
 
 function addPowerChart()
 {
-	powerChart = new Chart($("#powertoday"), {
-										type: "line",
-										data: powerdata,
-										options:{responsive: true,
-										         zoom:{enabled: true,
-										               mode:   'x',},
+    powerChart = new Chart($("#powertoday"), {
+                                        type: "line",
+                                        data: powerdata,
+                                        options:{responsive: true,
+                                                 zoom:{enabled: true,
+                                                       mode:   'x',},
                                        pan:{enabled:true,
                                             mode: 'x',},
                                        title:{
-                                       			display:true,
-                                       			text: 'Power production today',
-                                       		},
+                                                   display:true,
+                                                   text: 'Power production for ' + ((!!date)?date:'today'),
+                                               },
                                        tooltips:{
                                                 callbacks:
-                                                	{
-                                                	label: function(tooltipItems, data){
-                                                		return tooltipItems.yLabel + ' Watts';},
-                                                	}
+                                                    {
+                                                    label: function(tooltipItems, data){
+                                                        return tooltipItems.yLabel + ' Watts';},
+                                                    }
                                                 },
-									}}
-										);
-	$.getJSON("all-power.json?serial="+serial+"&todayonly=true")
+                                    }}
+                                        );
+    $.getJSON("all-power.json?serial="+serial+"&date="+date)
     .done (function(result) {
-    	  hideError();
+          hideError();
         loadNewPowerData(result);
        
     })
-    .fail (showError);									
+    .fail (showError);                                    
 }
 
 
 function loadNewPowerData(incoming)
 {
-	$.each(incoming.entries,function()
-	{
-		var time = new Date(0);
+    $.each(incoming.entries,function()
+    {
+        var time = new Date(0);
       time.setUTCSeconds(this[0]);
-		powerdata.labels.push(time.getHours() + ":"+ pad(time.getMinutes(),2));
-		
-		powerdata.datasets[0].data.push((this[1]));
-	}),powerChart.update();
-	
+        powerdata.labels.push(time.getHours() + ":"+ pad(time.getMinutes(),2));
+        
+        powerdata.datasets[0].data.push((this[1]));
+    }),powerChart.update();
+    
 }
 
 function updatePowerChart()
 {
-	$.getJSON("jeepingben.json")
+    if(date) {
+        hideError();
+        return;
+    }
+    $.getJSON("jeepingben.json")
     .done (function(result) {
-    	  hideError();
-    	  $.each(result.dvcs, function(){
-    	  
-    	  	if (this.s === serial)
-    	  	{
+          hideError();
+          $.each(result.dvcs, function(){
+          
+              if (this.s === serial)
+              {
             addPoint(this.p);
             
          }
@@ -84,12 +80,12 @@ function updatePowerChart()
 
 function addPoint(power)
 {
-	var time = new Date();
-	powerdata.labels.push(time.getHours() + ":"+pad(time.getMinutes(),2));
-	powerdata.datasets[0].data.push(power);
-	powerChart.update();
+    var time = new Date();
+    powerdata.labels.push(time.getHours() + ":"+pad(time.getMinutes(),2));
+    powerdata.datasets[0].data.push(power);
+    powerChart.update();
 }
-	
+    
 function pad (str, max) {
   str = str.toString();
   return str.length < max ? pad("0" + str, max) : str;
@@ -97,11 +93,41 @@ function pad (str, max) {
 
 function showError()
 {
-	$("#errormessage").show();
+    $("#errormessage").show();
 }
 function hideError()
 {
-	$("#errormessage").hide();
+    $("#errormessage").hide();
 }
 
-
+function changeDate()
+{
+    var newdate=new Date(Date.parse($("#datepicker").val()));
+    date=newdate.getFullYear()+""+pad(newdate.getMonth()+1, 2)+""+pad(newdate.getDate(),2);
+powerdata.labels=[];
+powerChart.options.title.text= 'Power production for ' + ((!!date)?date:'today');
+powerdata.datasets[0].data=[];
+    addPowerChart();
+}
+$(document).ready(function()
+{
+  var query = location.search.substr(1);
+  var result = {};
+  $( "#datepicker" ).datepicker({
+     onSelect: changeDate
+});
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    if (item[0] === 'serial') {
+       serial = item[1];
+    }
+    if (item[0] === 'date') {
+       date = item[1];
+    }
+  });
+    powerdata.datasets[0].label = serial;
+    addPowerChart();
+    hideError();
+    setInterval(updatePowerChart,300000); // new data available every 5 minutes
+}
+);
